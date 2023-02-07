0455c1a5b497782e8e4a154483a025e47d5aeef9
박용준 <lojugt98@gmail.com>
2023-02-07 17:54:09 +0900

    연습

--- Assets/Scripts/Park/MakeManager.cs
+++ Assets/Scripts/Park/MakeManager.cs
@@ -13,6 +13,11 @@ public class MakeManager : MonoBehaviour
     [SerializeField] Button btnPatty;
     [SerializeField] Button btnLowerBread;
     [SerializeField] Transform SpawnPoint;
+    [SerializeField] Button btnGameStart;
+    [SerializeField] Image questBox;
+
+    public WaitForSecondsRealtime wait5sec = new WaitForSecondsRealtime(5);
+    bool isStart;
 
     Vector3 defaultRotation = new Vector3(-90, 0, 0);
     // Start is called before the first frame update
@@ -27,6 +32,41 @@ public class MakeManager : MonoBehaviour
         
     }
 
+    public IEnumerator GetQuest()
+    {
+        while(isStart)
+        {
+            yield return wait5sec;
+
+            int ran = Random.Range(1, 11);
+            if(ran>7)
+            {
+                int ranidx = Random.Range(1, 5);
+                var ob = Resources.Load<GameObject>("Practice/quest");
+                var go = Instantiate(ob);
+                go.transform.SetParent(questBox.transform);
+                var quest = go.GetComponent<Quest>();
+                switch(ranidx)
+                {
+                    case 1:
+                        quest.txtQuest.text = "햄버거";
+                        break;
+                    case 2:
+                        quest.txtQuest.text = "치즈버거";
+                        break;
+                    case 3:
+                        quest.txtQuest.text = "비건버거";
+                        break;
+                    case 4:
+                        quest.txtQuest.text = "고기버거";
+                        break;
+
+                }
+            }
+        }
+
+    }
+
     private void SetButton()
     {
         btnUpperBread.onClick.AddListener(OnClickSpawnUpperBread);
@@ -35,8 +75,14 @@ public class MakeManager : MonoBehaviour
         btnCheese.onClick.AddListener(OnClickSpawnCheese);
         btnPatty.onClick.AddListener(OnClickSpawnPatty);
         btnLowerBread.onClick.AddListener(OnClickSpawnLowerBread);
+        btnGameStart.onClick.AddListener(OnClickGameStart);
     }
 
+    private void OnClickGameStart()
+    {
+        isStart = true;
+        StartCoroutine(GetQuest());
+    }
     private void OnClickSpawnUpperBread()
     {
         SpawnIngredients("UpperBread");
