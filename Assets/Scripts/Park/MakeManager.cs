520f068d83c37afd62a6ece823cd2894f47ad8ca
DESKTOP-QVOJ483\iibi203 <lojugt98@gmail.com>
2023-02-07 10:40:27 +0900

    연습중 추가 스크립트 생성

---
+++ Assets/Scripts/Park/MakeManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeManager : MonoBehaviour
+{
+
+    [SerializeField] Button btnUpperBread;
+    [SerializeField] Button btnLettuce;
+    [SerializeField] Button btnTomato;
+    [SerializeField] Button btnCheese;
+    [SerializeField] Button btnPatty;
+    [SerializeField] Button btnLowerBread;
+    [SerializeField] Transform SpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
