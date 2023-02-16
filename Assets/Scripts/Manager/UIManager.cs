287685d967122ed63808f62885c1f9d4647fce3b
KMR <ssal8888@naver.com>
2023-02-16 13:57:46 +0900

    //UIManager.cs 추가 //UIOrder 프리팹 추가

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour
+{
+    #region Singletone
+    private static UIManager instance;
+
+    public static UIManager GetInstance()
+    {
+        if(instance == null)
+        {
+            GameObject go = new GameObject("@UIManager");
+            instance = go.AddComponent<UIManager>();
+
+            DontDestroyOnLoad(go);
+        }
+        return instance;
+    }
+    #endregion
+
+    public void SetEventSystem()
+    {
+        if (FindObjectOfType<EventSystem>() == false)
+        {
+            GameObject go = new GameObject("@EventSystem");
+            go.AddComponent<EventSystem>();
+            go.AddComponent<StandaloneInputModule>();
+        }
+    }
+
+    public Dictionary<string, GameObject> uiList = new Dictionary<string, GameObject>();
+
+    public void OpenUI(string uiName)
+    {
+        if(uiList.ContainsKey(uiName) == false)
+        {
+            Object uiObj = Resources.Load("UI/" + uiName);
+            GameObject uiObject = (GameObject)Instantiate(uiObj);
+
+            uiList.Add(uiName, uiObject);
+        }
+        else
+            uiList[uiName].SetActive(true);
+    }
+
+    public void CloseUI(string uiName)
+    {
+        if (uiList.ContainsKey(uiName))
+            uiList[uiName].SetActive(false);
+    }
+
+    public void ClearList()
+    {
+        uiList.Clear();
+    }
+}
