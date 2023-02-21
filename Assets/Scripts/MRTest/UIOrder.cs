9e5d765f356d417fd65a9a34bec6ec17479059ed
KMR <ssal8888@naver.com>
2023-02-21 19:44:14 +0900

    //UIOrder 관련 수정중

--- Assets/Scripts/MRTest/UIOrder.cs
+++ Assets/Scripts/MRTest/UIOrder.cs
@@ -5,10 +5,21 @@ using UnityEngine.UI;
 
 public class UIOrder : MonoBehaviour
 {
-    public Text txtOrder;
+    public Text txtOrder1st;
+    public Text txtOrder2nd;
+    public Button[] btnUIOrder;
 
+    public void SetOrder(string pizzaName, int cnt)
+    {
+        txtOrder1st.text = $"{pizzaName} {cnt}ÆÇ";
+    }
+
+    public void SetOrder2nd(string pizzaName, int cnt)
+    {
+
+    }
     public Text OrderTxt()
     {
-        return txtOrder;
+        return txtOrder1st;
     }
 }
